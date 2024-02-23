d724613b1ce9503357f90651e7a51e2cc5461423
Hiperion09 <juanesteban.arenas2002@gmail.com>
2024-02-23 18:03:35 -0500

    Cambios 3.0

--- app/src/main/java/com/cristianzabala/ConstraintActivity.java
+++ app/src/main/java/com/cristianzabala/ConstraintActivity.java
@@ -3,14 +3,12 @@ package com.cristianzabala;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button back1, back2, back3;
+public class ConstraintActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_constraint);
     }
 }
