03887b318361240b9b7acb5d9b23b5f380afb6de
zZabalaZz <cristianzabala38@gmail.com>
2024-02-24 07:52:58 -0500

    g

--- app/src/main/java/com/cristianzabala/MainActivity.java
+++ app/src/main/java/com/cristianzabala/MainActivity.java
@@ -1,16 +1,47 @@
 package com.cristianzabala;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button back1, back2, back3;
+    Button cargar1, cargar2, cargar3;
 
-    @Override
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cargar1 = findViewById(R.id.load1);
+        cargar2 = findViewById(R.id.load2);
+        cargar3 = findViewById(R.id.load3);
+
+        cargar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ConstraintActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cargar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TableActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cargar3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
